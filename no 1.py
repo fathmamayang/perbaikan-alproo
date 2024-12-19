788ec90f499f4d8f2108af66476eb9f29d9469b5
fathmamayang <fathmamayang31@gmail.com>
2024-12-19 15:31:39 +0700

    first commit

---
+++ no 1.py
@@ -0,0 +1,7 @@
+n = int (input ("masukkan nilai n: "))
+total = 0
+i = 1
+for i in range(1, n+1) :
+    total += i
+i += 1
+print ("jumlah bilangan dari 1 hingga", n, "adalah: ", total)
---
+++ no 2.py
@@ -0,0 +1,4 @@
+import math
+r = float (input ("masukkan jari-jari lingkaran: "))
+luas = math.pi * r**2
+print ("luas lingkaran dengan jari-jari", r, "adalah: ", luas)
